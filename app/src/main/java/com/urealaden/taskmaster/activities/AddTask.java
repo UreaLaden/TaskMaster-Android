cacd90cb9958b9b5f9744d128158263ca43864a8
Leaundrae Mckinney <pytechdrae@gmail.com>
2021-05-11 22:12:02 -0400

    added s3 storage

--- app/src/main/java/com/urealaden/taskmaster/activities/AddTask.java
+++ app/src/main/java/com/urealaden/taskmaster/activities/AddTask.java
@@ -1,16 +1,20 @@
 package com.urealaden.taskmaster.activities;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.FileUtils;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -22,6 +26,10 @@ import com.amplifyframework.datastore.generated.model.Task;
 import com.amplifyframework.datastore.generated.model.Team;
 import com.urealaden.taskmaster.R;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -29,13 +37,13 @@ import static com.urealaden.taskmaster.activities.MainActivity.allTeams;
 
 public class AddTask extends AppCompatActivity {
 
+    static int GET_IMAGE_CODE = 97;
     public String TAG = "urealaden.signOut";
     Team selectedTeam;
     Handler handler;
     List<Task> totalTasks = new ArrayList<>();
-
-
-
+    File fileToUpload;
+    int imageNum = 1 ;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -74,9 +82,11 @@ public class AddTask extends AppCompatActivity {
         );
 
         submitButton.setOnClickListener( v-> {
+
             String title = ((EditText) findViewById(R.id.taskTitle)).getText().toString();
             String description = ((EditText) findViewById(R.id.taskDescription)).getText().toString();
             String team = ((TextView) findViewById(R.id.spinnerTextView)).getText().toString();
+
 
             for(Team t: allTeams){
                 if(team.contains(t.getName())){
@@ -84,7 +94,7 @@ public class AddTask extends AppCompatActivity {
                 }
             }
 
-           Task task = Task.builder()
+            Task task = Task.builder()
                    .name(title)
                    .description(description)
                    .team(selectedTeam)
@@ -99,9 +109,51 @@ public class AddTask extends AppCompatActivity {
 //                        Log.i(TAG, "onCreate: failed to add task");
                     }
             );
+
+
+
             Intent intent = new Intent(AddTask.this, MainActivity.class);
             AddTask.this.startActivity(intent);
             startActivity(intent);
+
+            saveFileToS3(fileToUpload,task.getId());
         });
+
+        Button addFileButton = findViewById(R.id.addFileButton);
+        addFileButton.setOnClickListener(v ->{
+            Intent intent = new Intent();
+            intent.setType("image/*");
+            intent.setAction(Intent.ACTION_GET_CONTENT);
+
+            startActivityForResult(intent,GET_IMAGE_CODE);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        imageNum += 1;
+        if(requestCode == GET_IMAGE_CODE){
+            //retrieve the file and upload it
+            fileToUpload = new File(getApplicationContext().getFilesDir(),"image-" + imageNum);
+            try{
+                InputStream inputStream = getContentResolver().openInputStream(data.getData());
+                FileUtils.copy(inputStream, new FileOutputStream(fileToUpload));
+                ImageView imageView = findViewById(R.id.addTaskImage);
+                imageView.setImageBitmap(BitmapFactory.decodeFile(fileToUpload.getPath()));
+
+            }catch(IOException e){
+                Log.e(TAG, "Failed to upload image successfully: " + e.toString());
+            }
+        }
+    }
+
+    void saveFileToS3(File file, String filename){
+        Amplify.Storage.uploadFile(
+                filename,
+                file,
+                r->{},
+                r ->{}
+        );
     }
 }
--- app/src/main/java/com/urealaden/taskmaster/activities/MainActivity.java
+++ app/src/main/java/com/urealaden/taskmaster/activities/MainActivity.java
@@ -1,20 +1,16 @@
 
 package com.urealaden.taskmaster.activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Handler;
 import android.os.Looper;
-import android.os.Message;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,28 +22,71 @@ import com.amplifyframework.auth.options.AuthSignOutOptions;
 import com.amplifyframework.core.Amplify;
 import com.amplifyframework.datastore.generated.model.Task;
 import com.amplifyframework.datastore.generated.model.Team;
+import com.amplifyframework.storage.s3.AWSS3StoragePlugin;
 import com.urealaden.taskmaster.R;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    public static String TAG = "urealadenTask.main";
+    public static String TAG = "TaskMaster.main";
     public List<Task> tasks = new ArrayList<>();
     public static List<Team> allTeams = new ArrayList<>();
 
 
-    void logoutCognito(){
+//  STORAGE STUFF
+
+    private void uploadFile(String key) {
+        File exampleFile = new File(getApplicationContext().getFilesDir(), "TestFile");
+
+        try {
+            BufferedWriter writer = new BufferedWriter(new FileWriter(exampleFile));
+            writer.append("I sure hope this works this time!!!");
+            writer.close();
+        } catch (Exception exception) {
+            Log.e(TAG, "Upload failed", exception);
+        }
+
+        Amplify.Storage.uploadFile(
+                key,
+                exampleFile,
+                result -> Log.i(TAG, "Successfully uploaded: " + result.getKey()),
+                storageFailure -> Log.e(TAG, "Upload failed", storageFailure)
+        );
+
+    }
+
+    private void downloadFile(String key) {
+        Amplify.Storage.downloadFile(
+                key,
+                new File(getApplicationContext().getFilesDir() + "/download.txt"),
+                result -> Log.i(TAG, "Successfully downloaded: " + result.getFile().getName()),
+                error -> Log.e(TAG, "Download Failure", error)
+        );
+
+        Amplify.Storage.getUrl(
+                key,
+                result -> Log.i(TAG, "Successfully generated: " + result.getUrl()),
+                error -> Log.e(TAG, "URL generation failure", error)
+        );
+    }
+//
+
+
+    void logoutCognito() {
         Amplify.Auth.signOut(
                 AuthSignOutOptions.builder()
                         .globalSignOut(true)
                         .build(),
                 () -> {
                     Looper.prepare();
-                    Toast.makeText(getApplicationContext(),"Signed out successfully", Toast.LENGTH_LONG).show();
+                    Toast.makeText(getApplicationContext(), "Signed out successfully", Toast.LENGTH_LONG).show();
                 },
-                error -> Log.e(TAG, "logoutCognito: unable to sign out" + error.toString() )
+                error -> Log.d(TAG, "logoutCognito: unable to sign out" + error.toString())
         );
     }
 
@@ -62,26 +101,31 @@ public class MainActivity extends AppCompatActivity {
         try {
             Amplify.addPlugin(new AWSApiPlugin());
             Amplify.addPlugin(new AWSCognitoAuthPlugin());
+            Amplify.addPlugin(new AWSS3StoragePlugin());
             Amplify.configure(getApplicationContext());
+
+            Log.i(TAG, "Initialized Amplify");
         } catch (AmplifyException e) {
-            e.printStackTrace();
+            Log.d(TAG, "Could not initialize Amplify ", e);
         }
 
+//        uploadFile("ExampleKey");
+//            downloadFile("ExampleKey");
 
-
-        String username = preferences.getString("username",null);
+        String username = preferences.getString("username", null);
         TextView usernameTextView = findViewById(R.id.textViewUsername);
-        if(username != null){
+        if (username != null) {
             usernameTextView.setText("Welcome back " + username + "!");
             usernameTextView.setVisibility(View.VISIBLE);
-        }else{
+        } else {
             usernameTextView.setVisibility(View.INVISIBLE);
         }
 
         Amplify.API.query(
                 ModelQuery.list(Task.class),
-                response ->{
-                    for(Task t: response.getData()){
+                response -> {
+                    Log.i(TAG, "Response: " + response.toString());
+                    for (Task t : response.getData()) {
                         tasks.add(t);
                     }
 
@@ -90,13 +134,11 @@ public class MainActivity extends AppCompatActivity {
         );
 
         Button loginButton = findViewById(R.id.loginButton);
-        loginButton.setOnClickListener(view ->{
-            startActivity(new Intent(MainActivity.this,Login.class));
+        loginButton.setOnClickListener(view -> {
+            startActivity(new Intent(MainActivity.this, Login.class));
         });
         Button logoutButton = findViewById(R.id.logoutButton);
-        logoutButton.setOnClickListener(view -> {
-            logoutCognito();
-        });
+        logoutButton.setOnClickListener(view -> {   logoutCognito();   });
 
         Button taskButton = findViewById(R.id.taskButton);
         taskButton.setOnClickListener(view -> {
@@ -104,18 +146,18 @@ public class MainActivity extends AppCompatActivity {
             startActivity(goToNewTaskPageIntent);
         });
         Button allTasksButton = findViewById(R.id.registerButton);
-        allTasksButton.setOnClickListener(view ->{
+        allTasksButton.setOnClickListener(view -> {
             Intent goToAllTaskPageIntent = new Intent(MainActivity.this, AllTasks.class);
             startActivity(goToAllTaskPageIntent);
         });
         Button signUpButton = findViewById(R.id.signupButton);
-        signUpButton.setOnClickListener(view ->{
+        signUpButton.setOnClickListener(view -> {
             Intent goToAllTaskPageIntent = new Intent(MainActivity.this, Register.class);
             startActivity(goToAllTaskPageIntent);
         });
 
         Button settingsButton = findViewById(R.id.settingsButton);
-        settingsButton.setOnClickListener(v ->{
+        settingsButton.setOnClickListener(v -> {
             Intent goToSettingsPageIntent = new Intent(MainActivity.this, Settings.class);
             startActivity(goToSettingsPageIntent);
         });
@@ -124,11 +166,12 @@ public class MainActivity extends AppCompatActivity {
                 ModelQuery.list(Team.class),
                 r -> {
                     allTeams.clear();
-                    for(Team t: r.getData()){
+                    for (Team t : r.getData()) {
                         allTeams.add(t);
                     }
                 },
-                r -> {}
+                r -> {
+                }
         );
 
     }
